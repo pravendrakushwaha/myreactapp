4dab00f3ded1de2442ebb590ac89111fa204d4e0
Pravendra <mr.parvender2011@gmail.com>
2024-02-08 21:42:45 +0530

    Intraction

---
+++ datatype.js
@@ -0,0 +1,45 @@
+//Datatypes
+
+//Numbers
+let age=24;
+const num2=23.42;
+console.log(age + ' ' +num2)
+console.log(`${age} ${num2}`)
+
+//Strings
+let str="pravendra";
+const str2='kushwaha';
+var str3 =`my name is ${str} ${str2}`;
+console.log(str + ' ' + str2);
+console.log(str3);
+
+//undefine and null
+let undf //first way to use undefine   
+let undf2=undefined //other way to use undefine
+let nu=null
+
+//Bolean
+let boln=false
+
+//Objects
+const person = {
+    fistname: 'Pravendra',
+    lastname: 'Kushwaha',
+    age: 24,
+    isOnline: true,
+    LastOnline: null,
+}
+
+console.log(person);
+console.log(person.fistname);
+console.log(person.isOnline);
+
+//NaN
+console.log('a' * 2);
+
+//Typeof Operator
+console.log(typeof (2));
+console.log(typeof ("pravendra"));
+console.log(typeof (true));
+
+
---
+++ intraction.js
@@ -0,0 +1,19 @@
+//Alert
+
+alert("Error Code 404");
+let alr="Bcoz of invalid input";
+let alr2=23;
+alert(`${alr} ${alr2}`);
+
+//Prompt
+
+let prp=prompt('How old are u');
+console.log(prp);
+let prp2=prompt('Where r u from')
+alert(`${prp} ${prp2}`);
+
+//Confirm
+
+let cnm=confirm("Are u new in JS");
+console.log(cnm);
+alert(`${cnm}`);
