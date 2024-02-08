eb52da852eb4c9aa2dbe671e5f31921675120dcd
Pravendra <mr.parvender2011@gmail.com>
2024-02-08 22:28:44 +0530

    puttin typeconv

---
+++ learnJS/typeconv.js
@@ -0,0 +1,21 @@
+//Type Conversion
+
+//String
+let str='2';
+let str2='40';
+console.log(`${str - str2}`);
+console.log('2'- 9);
+
+//Numaric
+let num='423';
+let num2='532';
+console.log(Number(num));
+console.log(Number(num2) - Number(num));
+
+//Boolean
+let bln=0;
+let bln2=' ';
+let bln3='';
+console.log(Boolean(bln));
+console.log(`${Boolean(bln2)}\n${Boolean(bln3)}`);
+
